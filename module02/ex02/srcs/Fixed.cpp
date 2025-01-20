fa295404aa994b122849e138a0cf44e3db658b7f
Clara <prando.c@live.fr>
2025-01-20 15:57:49 +0100

    module02ex02

---
+++ module02/ex02/srcs/Fixed.cpp
@@ -0,0 +1,153 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clara <clara@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 11:26:40 by clara             #+#    #+#             */
+/*   Updated: 2025/01/20 15:26:47 by clara            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _n(0){
+    // std::cout << "Default constructor called" << std::endl;
+    return ;
+}
+
+Fixed::Fixed(const int n){
+    // std::cout << "Parametric constructor called (int)" << std::endl;
+    _n = n * (1 << _fractionalBits);
+    return ;
+}
+
+Fixed::Fixed(const float f){
+    // std::cout << "Paramttric constructor called (float)" << std::endl;
+    _n = roundf(f * (1 << _fractionalBits));
+    return ;
+}
+
+Fixed::Fixed(Fixed const & src){
+    // std::cout << "Copy Constructor called" << std::endl;
+    *this = src;
+    return ;
+}
+
+Fixed::~Fixed(){
+    // std::cout << "Destructor called" << std::endl;
+    return ;
+}
+
+Fixed & Fixed::operator=(Fixed const & rhs){
+    // std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &rhs)
+		this->_n = rhs.getRawBits();
+    return *this;   
+}
+
+
+int Fixed::getValue() const{
+    return (this->_n);
+}
+
+int Fixed::getRawBits(void ) const {
+	return (this->_n);
+}
+
+void Fixed::setRawBits(int const raw) {
+	this->_n = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return (float)_n / (1 << _fractionalBits); 
+    // 1 << _fractionalBits = 2^8 = 256
+}
+
+int Fixed::toInt(void) const {
+	return (_n >> _fractionalBits);
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed) {
+	out << fixed.toFloat();
+	return (out);
+}
+
+bool Fixed::operator>(Fixed const & rhs) const{
+    return this->_n > rhs.getRawBits();
+}
+
+bool Fixed::operator<(Fixed const & rhs) const{
+    return this->_n < rhs.getRawBits();
+}
+
+bool Fixed::operator<=(Fixed const & rhs) const{
+    return this->_n <= rhs.getRawBits();
+}
+
+bool Fixed::operator>=(Fixed const & rhs) const{
+    return this->_n >= rhs.getRawBits();
+}
+
+bool Fixed::operator==(Fixed const & rhs) const{
+    return this->_n == rhs.getRawBits();
+}
+
+bool Fixed::operator!=(Fixed const & rhs) const{
+    return (this->_n != rhs.getRawBits());
+}
+
+Fixed Fixed::operator+(Fixed const & rhs) const{
+    return Fixed(this->toFloat() + rhs.toFloat());;
+}
+
+Fixed Fixed::operator-(Fixed const & rhs) const{
+    return Fixed( this->toFloat() - rhs.toFloat() ) ;
+}
+
+Fixed Fixed::operator/(Fixed const & rhs) const{
+    return Fixed( this->toFloat() / rhs.toFloat() ) ;
+}
+
+Fixed Fixed::operator*(Fixed const & rhs) const{
+    return Fixed( this->toFloat() * rhs.toFloat() ) ;
+}
+
+Fixed &	Fixed::operator++(void){
+	this->_n++ ;
+	return *this ;
+}
+
+Fixed &	Fixed::operator--(void){
+	this -> _n-- ;
+	return *this ;
+}
+
+Fixed Fixed::operator++(int){
+	Fixed tmp(*this) ;
+	++(*this);
+	return tmp ;
+}
+
+Fixed Fixed::operator--(int){
+	Fixed tmp(*this) ;
+	--(*this);
+	return tmp ;
+}
+
+Fixed & Fixed::min(Fixed & a, Fixed & b){
+	return a < b ? a : b ;
+}
+
+Fixed & Fixed::max(Fixed & a, Fixed & b){
+	return a > b ? a : b ;
+}
+
+const Fixed & Fixed::min(const Fixed & a, const Fixed & b){
+	return a < b ? a : b ;
+}
+
+const Fixed & Fixed::max(const Fixed & a, const Fixed & b){
+	return a > b ? a : b ;
+}
---
+++ module02/ex02/srcs/main.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clara <clara@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 11:24:46 by clara             #+#    #+#             */
+/*   Updated: 2025/01/20 15:54:46 by clara            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+// int main( void ) {
+//     std::cout << "TEST 1" << std::endl;
+//     Fixed a;
+//     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+//     std::cout << a << std::endl;
+//     std::cout << ++a << std::endl;
+//     std::cout << a << std::endl;
+//     std::cout << a++ << std::endl;
+//     std::cout << a << std::endl;
+//     std::cout << b << std::endl;
+//     std::cout << Fixed::max( a, b ) << std::endl;
+    
+
+//     std::cout << "TEST 2" << std::endl;
+//     Fixed c;
+    
+//     return 0;
+// }
+
+int main(void) {
+    std::cout << "--- Initialisation ---" << std::endl;
+    Fixed a;
+    Fixed b(5);
+    Fixed c(5.05f);
+    std::cout << "a (default)     : " << a << std::endl; 
+    std::cout << "b (int=5)       : " << b << std::endl; 
+    std::cout << "c (float=5.05)  : " << c << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "\n--- Incrémentation ---" << std::endl;
+    std::cout << "a               : " << a << std::endl;
+    std::cout << "a++ (postfixe)  : " << a++ << std::endl;
+    std::cout << "a (après a++)   : " << a << std::endl;
+    std::cout << "++a (préfixe)   : " << ++a << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "\n--- Opérations arithmétiques ---" << std::endl;
+    Fixed d = b + c; // 5 + 5.05
+    Fixed e = b - c; // 5 - 5.05
+    Fixed f = b * c; // 5 * 5.05
+    Fixed g = b / c; // 5 / 5.05
+
+    std::cout << "b + c           : " << d << std::endl;  // 10.05
+    std::cout << "b - c           : " << e << std::endl;  // -0.05
+    std::cout << "b * c           : " << f << std::endl;  // 25.25
+    std::cout << "b / c           : " << g << std::endl;  // ~0.9901
+
+    std::cout << std::endl;
+    std::cout << "\n--- Comparaisons ---" << std::endl;
+    std::cout << "b > c           : " << (b > c) << std::endl;  // 0 (faux)
+    std::cout << "b < c           : " << (b < c) << std::endl;  // 1 (vrai)
+    std::cout << "b == c          : " << (b == c) << std::endl; // 0 (faux)
+    std::cout << "b != c          : " << (b != c) << std::endl; // 1 (vrai)
+
+    std::cout << std::endl;
+    std::cout << "\n--- Min/Max ---" << std::endl;
+    std::cout << "Min(b, c)       : " << Fixed::min(b, c) << std::endl;  // 5
+    std::cout << "Max(b, c)       : " << Fixed::max(b, c) << std::endl;  // 5.05
+    return 0;
+}
+
